6a204f4aed3266dc861e3d52b107f2b4cc9894e9
M. Toups <mtoups3@tulane.edu>
2024-04-23 15:27:00 -0500

    skip running apt-get ... surely this will speed up starting this up

--- profile.py
+++ profile.py
@@ -26,7 +26,7 @@ request = pc.makeRequestRSpec()
 node_router_a = request.XenVM('router-a')
 node_router_a.disk_image = 'urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU22-64-STD'
 node_router_a.Site('Site 1')
-node_router_a.addService(pg.Execute('/bin/sh','sudo apt-get update; sudo apt-get -y install mtr'))
+#node_router_a.addService(pg.Execute('/bin/sh','sudo apt-get update; sudo apt-get -y install mtr'))
 node_router_a.addService(pg.Execute('/bin/sh','sudo sysctl -w net.ipv4.ip_forward=1'))
 iface0 = node_router_a.addInterface('interface-5', pg.IPv4Address('0.0.0.0','0.0.0.0'))
 iface1 = node_router_a.addInterface('interface-0', pg.IPv4Address('10.1.10.1','255.255.255.0'))
@@ -35,7 +35,7 @@ iface1 = node_router_a.addInterface('interface-0', pg.IPv4Address('10.1.10.1','2
 node_router_b = request.XenVM('router-b')
 node_router_b.disk_image = 'urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU22-64-STD'
 node_router_b.Site('Site 1')
-node_router_b.addService(pg.Execute('/bin/sh','sudo apt-get update; sudo apt-get -y install mtr'))
+#node_router_b.addService(pg.Execute('/bin/sh','sudo apt-get update; sudo apt-get -y install mtr'))
 node_router_b.addService(pg.Execute('/bin/sh','sudo sysctl -w net.ipv4.ip_forward=1'))
 iface2 = node_router_b.addInterface('interface-2', pg.IPv4Address('10.1.10.2','255.255.255.0'))
 iface3 = node_router_b.addInterface('interface-9', pg.IPv4Address('0.0.0.0','0.0.0.0'))
@@ -44,7 +44,7 @@ iface3 = node_router_b.addInterface('interface-9', pg.IPv4Address('0.0.0.0','0.0
 node_router_c = request.XenVM('router-c')
 node_router_c.disk_image = 'urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU22-64-STD'
 node_router_c.Site('Site 1')
-node_router_c.addService(pg.Execute('/bin/sh','sudo apt-get update; sudo apt-get -y install mtr'))
+#node_router_c.addService(pg.Execute('/bin/sh','sudo apt-get update; sudo apt-get -y install mtr'))
 node_router_c.addService(pg.Execute('/bin/sh','sudo sysctl -w net.ipv4.ip_forward=1'))
 iface4 = node_router_c.addInterface('interface-3', pg.IPv4Address('10.1.10.3','255.255.255.0'))
 iface5 = node_router_c.addInterface('interface-13', pg.IPv4Address('0.0.0.0','0.0.0.0'))
@@ -53,7 +53,7 @@ iface5 = node_router_c.addInterface('interface-13', pg.IPv4Address('0.0.0.0','0.
 node_juliet = request.XenVM('juliet')
 node_juliet.disk_image = 'urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU22-64-STD'
 node_juliet.Site('Site 1')
-node_juliet.addService(pg.Execute('/bin/sh','sudo apt-get update; sudo apt-get -y install mtr'))
+#node_juliet.addService(pg.Execute('/bin/sh','sudo apt-get update; sudo apt-get -y install mtr'))
 node_juliet.addService(pg.Execute('/bin/sh','wget -O - https://git.io/vSWTX | bash'))
 iface6 = node_juliet.addInterface('interface-6', pg.IPv4Address('0.0.0.0','0.0.0.0'))
 
@@ -61,7 +61,7 @@ iface6 = node_juliet.addInterface('interface-6', pg.IPv4Address('0.0.0.0','0.0.0
 node_romeo = request.XenVM('romeo')
 node_romeo.disk_image = 'urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU22-64-STD'
 node_romeo.Site('Site 1')
-node_romeo.addService(pg.Execute('/bin/sh','sudo apt-get update; sudo apt-get -y install mtr'))
+#node_romeo.addService(pg.Execute('/bin/sh','sudo apt-get update; sudo apt-get -y install mtr'))
 node_romeo.addService(pg.Execute('/bin/sh','wget -O - https://git.io/vSWTX | bash'))
 iface7 = node_romeo.addInterface('interface-4', pg.IPv4Address('0.0.0.0','0.0.0.0'))
 
@@ -69,7 +69,7 @@ iface7 = node_romeo.addInterface('interface-4', pg.IPv4Address('0.0.0.0','0.0.0.
 node_othello = request.XenVM('othello')
 node_othello.disk_image = 'urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU22-64-STD'
 node_othello.Site('Site 1')
-node_othello.addService(pg.Execute('/bin/sh','sudo apt-get update; sudo apt-get -y install mtr'))
+# node_othello.addService(pg.Execute('/bin/sh','sudo apt-get update; sudo apt-get -y install mtr'))
 node_othello.addService(pg.Execute('/bin/sh','wget -O - https://git.io/vSWTX | bash'))
 iface8 = node_othello.addInterface('interface-8', pg.IPv4Address('0.0.0.0','0.0.0.0'))
 
@@ -77,7 +77,7 @@ iface8 = node_othello.addInterface('interface-8', pg.IPv4Address('0.0.0.0','0.0.
 node_desdemona = request.XenVM('desdemona')
 node_desdemona.disk_image = 'urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU22-64-STD'
 node_desdemona.Site('Site 1')
-node_desdemona.addService(pg.Execute('/bin/sh','sudo apt-get update; sudo apt-get -y install mtr'))
+#node_desdemona.addService(pg.Execute('/bin/sh','sudo apt-get update; sudo apt-get -y install mtr'))
 node_desdemona.addService(pg.Execute('/bin/sh','wget -O - https://git.io/vSWTX | bash'))
 iface9 = node_desdemona.addInterface('interface-10', pg.IPv4Address('0.0.0.0','0.0.0.0'))
 
@@ -85,7 +85,7 @@ iface9 = node_desdemona.addInterface('interface-10', pg.IPv4Address('0.0.0.0','0
 node_ophelia = request.XenVM('ophelia')
 node_ophelia.disk_image = 'urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU22-64-STD'
 node_ophelia.Site('Site 1')
-node_ophelia.addService(pg.Execute('/bin/sh','sudo apt-get update; sudo apt-get -y install mtr'))
+#node_ophelia.addService(pg.Execute('/bin/sh','sudo apt-get update; sudo apt-get -y install mtr'))
 node_ophelia.addService(pg.Execute('/bin/sh','wget -O - https://git.io/vSWTX | bash'))
 iface10 = node_ophelia.addInterface('interface-14', pg.IPv4Address('0.0.0.0','0.0.0.0'))
 
@@ -93,7 +93,7 @@ iface10 = node_ophelia.addInterface('interface-14', pg.IPv4Address('0.0.0.0','0.
 node_hamlet = request.XenVM('hamlet')
 node_hamlet.disk_image = 'urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU22-64-STD'
 node_hamlet.Site('Site 1')
-node_hamlet.addService(pg.Execute('/bin/sh','sudo apt-get update; sudo apt-get -y install mtr'))
+#node_hamlet.addService(pg.Execute('/bin/sh','sudo apt-get update; sudo apt-get -y install mtr'))
 node_hamlet.addService(pg.Execute('/bin/sh','wget -O - https://git.io/vSWTX | bash'))
 iface11 = node_hamlet.addInterface('interface-12', pg.IPv4Address('0.0.0.0','0.0.0.0'))
 
